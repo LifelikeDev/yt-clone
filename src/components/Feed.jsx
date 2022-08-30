cb843a35aadc79a611be0b42cdc341010be9e0d0
LifelikeDev <primeguyy@gmail.com>
2022-08-30 21:41:52 +0000

    sidebar added

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Stack, Box, Typography } from "@mui/material";
+import Sidebar from "./Sidebar"
 
 const Feed = () => {
   return (
-    <div>Feed</div>
-  )
-}
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5 }}>
+          &copy; LifelikeDev {new Date().getFullYear()}
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
 
-export default Feed
+export default Feed;
