1133441c70e86509efef0562281926407220a3cb
LifelikeDev <primeguyy@gmail.com>
2022-09-02 23:11:59 +0000

    channel profile init and videos loading

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/system";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+  const [channelContent, setChannelContent] = useState(null);
+  const [channelVideos, setChannelVideos] = useState([]);
+  const { id } = useParams();
 
-export default ChannelDetail
+  console.log({ channelContent });
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part="snippet&id=${id}`).then((data) =>
+      setChannelContent(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setChannelVideos(data?.items)
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)",
+            height: "300px",
+            zIndex: 10,
+          }}
+        />
+        <ChannelCard channelDetail={channelContent} />
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelDetail;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Stack, Box } from "@mui/system";
 import { ChannelCard, VideoCard } from "./";
+import { Typography } from "@mui/material";
 
 const Videos = ({ videos }) => {
   return (
     <Stack direction="row" flexWrap="wrap" gap={2} justifyContent="start">
-      {videos.map((item, idx) => (
-        <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item} />}
-          {item.id.channelId && <ChannelCard channelDetail={item} />}
-        </Box>
-      ))}
+      {!videos.length ? (
+       
+          <Typography variant="h5" textAlign="center" margin="25vh auto">L o a d i n g . . .</Typography>
+      ) : (
+        videos.map((item, idx) => (
+          <Box key={idx}>
+            {item.id.videoId && <VideoCard video={item} />}
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
+          </Box>
+        ))
+      )}
     </Stack>
   );
 };
