cc4ce896d56fc6b44acda1ed79c9017b63ead93d
LifelikeDev <primeguyy@gmail.com>
2022-09-03 22:27:17 +0000

    show upload date and time of video

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle"
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 import {
   demoThumbnailUrl,
@@ -18,7 +18,13 @@ const VideoCard = ({
   },
 }) => {
   return (
-    <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
+    <Card
+      sx={{
+        width: { xs: "100%", sm: "358px", md: "320px" },
+        boxShadow: "none",
+        borderRadius: 0,
+      }}
+    >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
@@ -26,18 +32,49 @@ const VideoCard = ({
           sx={{ width: { xs: "100%", sm: "358px" }, height: 180 }}
         />
       </Link>
-      <CardContent sx={{ backgroundColor: "#282828", height: "106px" }}>
+      <CardContent
+        sx={{
+          backgroundColor: "#282828",
+          height: "106px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
+        }}
+      >
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
           <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
-            {`${snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}...`}
+            {`${
+              snippet?.title.slice(0, 50).trim() ||
+              demoVideoTitle.slice(0, 60).trim()
+            }...`}
           </Typography>
         </Link>
-        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
-        <Typography variant="subtitle2" color="gray">
-          {snippet?.channelTitle || demoChannelTitle}
-          <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography variant="subtitle2" color="#bbb">
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircleIcon
+              sx={{ fontSize: "12px", color: "#aaa", ml: "5px" }}
+            />
+          </Typography>
+        </Link>
+        <Typography
+          variant="subtitle2"
+          fontSize="13px"
+          color="#aaa"
+          marginY="10px"
+        >
+          {`${new Date(snippet?.publishedAt)
+            .toDateString()
+            .substring(4)} at ${snippet?.publishedAt
+            .split("T")[1]
+            .slice(0, -4)}`}
         </Typography>
-      </Link>
       </CardContent>
     </Card>
   );
